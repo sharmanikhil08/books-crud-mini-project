7b5d6960fc7131f29790006b419ca22a800c593e
Nikhil Sharma <sharma.nikhil@live.com>
2021-06-16 23:49:00 -0300

    Add book model entity

---
+++ Books-Crud-API/Models/Book.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Books_Crud_API.Models
+{
+    public class Book: BaseEntity
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int PublicationYear { get; set; }
+        public bool IsAvailable { get; set; }
+    }
+}
