c7374eda4de430a0286f999697dd72eb87d5d3b5
Nikhil Sharma <sharma.nikhil@live.com>
2021-06-16 23:53:37 -0300

    Clean up unused headers.

--- Books-Crud-API/Models/Book.cs
+++ Books-Crud-API/Models/Book.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace Books_Crud_API.Models
+﻿namespace Books_Crud_API.Models
 {
     public class Book: BaseEntity
     {
