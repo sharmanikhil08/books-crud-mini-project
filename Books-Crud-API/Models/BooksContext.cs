50570b9f939bf80d47edb03e8183c18b91e5fc49
Nikhil Sharma <sharma.nikhil@live.com>
2021-06-16 23:55:49 -0300

    Add application db context and hook it up in startup.

---
+++ Books-Crud-API/Models/BooksContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Books_Crud_API.Models
+{
+    public class BooksContext : DbContext
+    {
+        public BooksContext(DbContextOptions<BooksContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
--- Books-Crud-API/Startup.cs
+++ Books-Crud-API/Startup.cs
@@ -1,16 +1,11 @@
+using Books_Crud_API.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Books_Crud_API
 {
@@ -26,7 +21,9 @@ namespace Books_Crud_API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            // Hookup application DB Context with in memory DB.
+            services.AddDbContext<BooksContext>(opt =>
+                                               opt.UseInMemoryDatabase("BooksDB"));
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
