c3b4fed7a85924d80eeae199e475391ab3c781a7
Nikhil Sharma <sharma.nikhil@live.com>
2021-06-17 02:24:25 -0300

    Add new component AddBook with button and blank modal to add new book entry.

---
+++ books-crud-frontend/src/components/AddBook.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <b-container>
+      <b-button v-b-modal.modal-center variant="success" class="add-button"
+        >Add new book</b-button
+      >
+      <b-modal id="modal-center" centered title="Add new book"> </b-modal>
+    </b-container>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.add-button {
+  float: left;
+}
+</style>
--- books-crud-frontend/src/views/Home.vue
+++ books-crud-frontend/src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="home">
     <h1 class="page-header">BOOKS</h1>
+    <add-book></add-book>
     <books-grid></books-grid>
   </div>
 </template>
@@ -8,15 +9,20 @@
 <script>
 // @ is an alias to /src
 import BooksGrid from "@/components/BooksGrid.vue";
+import AddBook from "@/components/AddBook.vue";
 
 export default {
   name: "Home",
-  components: { "books-grid": BooksGrid },
+  components: { "books-grid": BooksGrid, "add-book": AddBook },
 };
 </script>
 
 <style>
 .page-header {
   margin-top: 50px;
+}
+
+.modal-title {
+  margin: auto;
 }
 </style>
